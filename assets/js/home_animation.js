a82d8b8760b66522cffe00136b33bbb599183759
Nick Li <nick.li@SAP-MAC-02.local>
2026-02-06 15:30:40 +0800

    process 2 o

--- assets/js/home_animation.js
+++ assets/js/home_animation.js
@@ -1,42 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  animationFirst()
+  animationSecond()
+})
 
+function animationFirst() {
   const t1 = gsap.timeline({
     defaults: { ease: "none" },
     scrollTrigger: {
       trigger: "#banner-1",
       start: "top top",
-      // end: "+=400%",
+      end: "+=600%",
       pin: true,
       scrub: 1.5,
       anticipatePin: 1,
       markers: true // debug usage
     }
   })
-
-  const t2 = gsap.timeline({
-    defaults: { ease: "none" },
-    scrollTrigger: {
-      trigger: "#banner-2",
-      start: "top top",
-      end: "+=400%",
-      pin: true,
-      scrub: 1.5,
-      anticipatePin: 1,
-      markers: true // debug usage
-    }
-  })
-
-  animationFirst(t1)
-  animationSecond(t2)
-
-})
-function animationFirst(t1) {
   // step 1
   t1.to("#banner-1-img", {
-    scale: 1.4,
-    x: 500,
-    y: 820,
+    scale: 1.2,
+    x: '20%',
+    y: '70%',
     duration: 3,
     onEnter: () => t1.scrollTrigger.spacer.style.backgroundColor = "#fcdfe2"
   }, "+=1.5")
@@ -48,9 +33,12 @@ function animationFirst(t1) {
     x: 0,
     y: 300,
     duration: 4,
-    onEnter: () => t1.scrollTrigger.spacer.style.backgroundColor = "#fcdfe2"
   }, "+=1.2")
-  t1.to("#card-1-2", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
+  t1.to("#card-1-2", {
+    autoAlpha: 1,
+    duration: 2.2,
+    ease: "power1.out",
+  }, "+=0.4")
   t1.to("#card-1-2", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
   // step 3
   t1.to("#banner-1-img", {
@@ -58,7 +46,6 @@ function animationFirst(t1) {
     x: -300,
     y: 900,
     duration: 4,
-    onEnter: () => t1.scrollTrigger.spacer.style.backgroundColor = "#fcdfe2"
   }, "+=1.2")
   t1.to("#card-1-3", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
   t1.to("#card-1-3", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
@@ -71,7 +58,19 @@ function animationFirst(t1) {
   }, "+=1")
 }
 
-function animationSecond(t2) {
+function animationSecond() {
+  const t2 = gsap.timeline({
+    defaults: { ease: "none" },
+    scrollTrigger: {
+      trigger: "#banner-2",
+      start: "top top",
+      end: "+=600%",
+      pin: true,
+      scrub: 1.5,
+      anticipatePin: 1,
+      markers: true // debug usage
+    }
+  })
   // step 1
   t2.to("#banner-2-img", {
     scale: 1.4,
@@ -88,7 +87,6 @@ function animationSecond(t2) {
     x: 700,
     y: 200,
     duration: 4,
-    onEnter: () => t2.scrollTrigger.spacer.style.backgroundColor = "#edd7c3"
   }, "+=1.2")
   t2.to("#card-2", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
   t2.to("#card-2", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
@@ -98,7 +96,6 @@ function animationSecond(t2) {
     x: -200,
     y: 380,
     duration: 4,
-    onEnter: () => t2.scrollTrigger.spacer.style.backgroundColor = "#edd7c3"
   }, "+=1.2")
   t2.to("#card-3", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
   t2.to("#card-3", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
@@ -108,7 +105,6 @@ function animationSecond(t2) {
     x: -450,
     y: 950,
     duration: 4,
-    onEnter: () => t2.scrollTrigger.spacer.style.backgroundColor = "#edd7c3"
   }, "+=1.2")
   t2.to("#card-4", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
   t2.to("#card-4", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
@@ -118,7 +114,6 @@ function animationSecond(t2) {
     x: -300,
     y: 0,
     duration: 4,
-    onEnter: () => t2.scrollTrigger.spacer.style.backgroundColor = "#edd7c3"
   }, "+=1.2")
   t2.to("#card-5", { autoAlpha: 1, duration: 2.2, ease: "power1.out" }, "+=0.4")
   t2.to("#card-5", { autoAlpha: 0, duration: 1.8, ease: "power1.in" }, "+=0.8")
@@ -132,5 +127,5 @@ function animationSecond(t2) {
 }
 
 function animationThird() {
-  
+
 }
